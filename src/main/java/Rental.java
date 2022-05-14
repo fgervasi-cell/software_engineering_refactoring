bce86b8c8d81badf3cf672b5b2c956f9a158f787
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2022-05-14 13:22:25 +0200

    Move: Move Method
    Moved method Rental.getFrequentRenterPoints() to Movie class and used Movie's new method inside Rental. This is because the data used in this method lies mostly inside the Movie class.

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -17,9 +17,6 @@ class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        if (this.getMovie().getPriceCode() == Movie.NEW_RELEASE && this.getDaysRented() > 1) {
-            return 2;
-        }
-        return 1;
+        return this.movie.getFrequentRenterPoints(this.daysRented);
     }
 }
