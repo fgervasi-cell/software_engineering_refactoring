d9bdc2e646d919b3b30bda55a21aac402c310eb5
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2022-05-14 13:42:05 +0200

    Move: Move Method
    Move method 'getCharge' from Movie class to Price class and used Price's new method in Movie.

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -1,3 +1,21 @@
 public abstract class Price {
     abstract int getPriceCode();
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR -> {
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+            }
+            case Movie.NEW_RELEASE -> result += daysRented * 3;
+            case Movie.CHILDREN -> {
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+            }
+        }
+        return result;
+    }
 }
