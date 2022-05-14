14e14be003443dced322ab80d9796d5f45688d0a
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2022-05-14 12:52:13 +0200

    Added method Customer.htmlStatement().

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -9,19 +9,13 @@ class Customer {
         this.name = name;
     }
 
-    ;
-
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     }
 
-    ;
-
     public String getName() {
         return name;
     }
-
-    ;
 
     public String statement() {
         Enumeration<Rental> enum_rentals = rentals.elements();
@@ -31,11 +25,25 @@ class Customer {
         while (enum_rentals.hasMoreElements()) {
             Rental each = enum_rentals.nextElement();
             //show figures for this rental
-            result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(String.valueOf(each.getCharge())).append("\n");
+            result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(each.getCharge()).append("\n");
         }
         //add footer lines
-        result.append("Amount owed is ").append(String.valueOf(this.getTotalCharge())).append("\n");
-        result.append("You earned ").append(String.valueOf(this.getTotalFrequentRenterPoints())).append(" frequent renter points");
+        result.append("Amount owed is ").append(this.getTotalCharge()).append("\n");
+        result.append("You earned ").append(this.getTotalFrequentRenterPoints()).append(" frequent renter points");
+        return result.toString();
+    }
+
+    public String htmlStatement() {
+        Enumeration<Rental> rentals = this.rentals.elements();
+        StringBuilder result = new StringBuilder("<h1>Rentals for <em>" + getName() + "</em></h1><p>\n");
+        while(rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            // show figures for each rental
+            result.append(each.getMovie().getTitle()).append(": ").append(each.getCharge()).append("<br>\n");
+        }
+        // add footer lines
+        result.append("<p>You owe <em>").append(this.getTotalCharge()).append("</em><p>\n");
+        result.append("On this rental you earned <em>").append(this.getTotalFrequentRenterPoints()).append("</em> frequent renter points<p>");
         return result.toString();
     }
 
--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -15,19 +15,17 @@ class Rental {
     public double getCharge() {
         double result = 0;
         switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
+            case Movie.REGULAR -> {
                 result += 2;
                 if (this.getDaysRented() > 2)
                     result += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
+            }
+            case Movie.NEW_RELEASE -> result += this.getDaysRented() * 3;
+            case Movie.CHILDREN -> {
                 result += 1.5;
                 if (this.getDaysRented() > 3)
                     result += (this.getDaysRented() - 3) * 1.5;
-                break;
+            }
         }
         return result;
     }
